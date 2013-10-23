7476727308a1576bf8c282a12818509bd299864a
Aaron Parecki <aaron@parecki.com>
2013-10-22 21:09:24 -0700

    add LOLDB

---
+++ OfflineLogger/LOLDB/LOLDatabase.h
@@ -0,0 +1,39 @@
+//
+//  LOLDatabase.h
+//  loldb
+//
+//  Created by Andrew Pouliot on 12/12/11.
+//  Copyright (c) 2011 Geoloqi, Inc. All rights reserved.
+//
+
+/*
+ * Design goals:
+ * 1. Getting or storing an item out is < O(n) and as close to O(1) as possible
+ * 2. Never keep an unbounded set in memory!
+ * 3. Allow seamless access from multiple threads via dispatch api
+ */
+
+#import <Foundation/Foundation.h>
+
+@protocol LOLDatabaseAccessor <NSObject>
+
+//Uses JSON to serialize and deserialize from the database
+- (NSDictionary *)dictionaryForKey:(NSString *)key;
+- (void)setDictionary:(NSDictionary *)dict forKey:(NSString *)key;
+- (void)removeDictionaryForKey:(NSString *)key;
+
+- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(NSString *key, NSDictionary *object, BOOL *stop))block;
+
+@end
+
+@interface LOLDatabase : NSObject
+
+- (id)initWithPath:(NSString *)path;
+
+//You must must fill these out to define the method by which objects are serialized to/from data
+@property (copy) NSData *(^serializer)(id object);
+@property (copy) id (^deserializer)(NSData *data);
+
+- (void)accessCollection:(NSString *)collection withBlock:(void (^)(id <LOLDatabaseAccessor>accessor))block;
+
+@end
