495937ffce3fecb3e678873cc0797e8c2d4bdb76
Aaron Parecki <aaron@parecki.com>
2023-12-16 07:23:43 -0800

    reorder properties to match UI

--- GPSLogger/SettingsViewController.h
+++ GPSLogger/SettingsViewController.h
@@ -12,33 +12,33 @@
 
 @property (strong, nonatomic) IBOutlet UILabel *apiEndpointField;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *trackingEnabledToggle;
-@property (strong, nonatomic) IBOutlet UISegmentedControl *pausesAutomatically;
-@property (strong, nonatomic) IBOutlet UISegmentedControl *resumesWithGeofence;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *continuousTrackingMode;
-@property (strong, nonatomic) IBOutlet UISegmentedControl *activityType;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *desiredAccuracy;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *activityType;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *showBackgroundLocationIndicator;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *pausesAutomatically;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *loggingMode;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *pointsPerBatchControl;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *resumesWithGeofence;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *discardPointsWithinDistance;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *discardPointsWithinSeconds;
-@property (strong, nonatomic) IBOutlet UISegmentedControl *pointsPerBatchControl;
-@property (strong, nonatomic) IBOutlet UISegmentedControl *showBackgroundLocationIndicator;
 @property (strong, nonatomic) IBOutlet UISwitch *enableNotifications;
-@property (strong, nonatomic) IBOutlet UISegmentedControl *loggingMode;
 @property (strong, nonatomic) IBOutlet UIStackView *locationAuthorizationStatusSection;
 @property (strong, nonatomic) IBOutlet UILabel *locationAuthorizationStatus;
 @property (strong, nonatomic) IBOutlet UILabel *locationAuthorizationStatusWarning;
 @property (strong, nonatomic) IBOutlet UIButton *requestLocationPermissionsButton;
 
 - (IBAction)toggleLogging:(UISegmentedControl *)sender;
-- (IBAction)pausesAutomaticallyWasChanged:(UISegmentedControl *)sender;
-- (IBAction)resumeWithGeofenceWasChanged:(UISegmentedControl *)sender;
 - (IBAction)continuousTrackingModeWasChanged:(UISegmentedControl *)sender;
-- (IBAction)activityTypeControlWasChanged:(UISegmentedControl *)sender;
 - (IBAction)desiredAccuracyWasChanged:(UISegmentedControl *)sender;
+- (IBAction)activityTypeControlWasChanged:(UISegmentedControl *)sender;
+- (IBAction)showBackgroundLocationIndicatorWasChanged:(UISegmentedControl *)sender;
+- (IBAction)pausesAutomaticallyWasChanged:(UISegmentedControl *)sender;
+- (IBAction)loggingModeWasChanged:(UISegmentedControl *)sender;
 - (IBAction)pointsPerBatchWasChanged:(UISegmentedControl *)sender;
+- (IBAction)resumeWithGeofenceWasChanged:(UISegmentedControl *)sender;
 - (IBAction)discardPointsWithinDistanceWasChanged:(UISegmentedControl *)sender;
 - (IBAction)discardPointsWithinSecondsWasChanged:(UISegmentedControl *)sender;
-- (IBAction)showBackgroundLocationIndicatorWasChanged:(UISegmentedControl *)sender;
-- (IBAction)loggingModeWasChanged:(UISegmentedControl *)sender;
 - (IBAction)toggleNotificationsEnabled:(UISwitch *)sender;
 - (IBAction)requestLocationPermissionsWasPressed:(UIButton *)sender;
 
